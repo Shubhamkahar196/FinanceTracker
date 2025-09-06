95f7577672d5632a4dd0337d6427a0de73e2be44
Shubham kahar <Shubhamkahar196@gmail.com>
2025-09-06 17:49:49 +0530

    making a personalFinance Tracker

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import  express from 'express';
+import connectDb from './config/db.js';
+import dotenv from 'dotenv'
+
+const app = express();
+  
+dotenv.config();
+connectDb();   //connecting db
+app.listen(8080,()=>{
+    console.log("Server is running on port 8080");
+    
+})
---
+++ backend/src/models/transaction.model.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+export interface ITransaction extends Document{
+    amount: number;
+    type: 'income' | 'expense';
+    category: string;
+    date: Date;
+    description?: string;
+    userId: string;
+}
+
+const transactionSchema = new mongoose.Schema<ITransaction>({
+   amount: {
+    type: Number,
+    required: [true, 'Amount is required'],
+   },
+   type: {
+    type: String,
+    enum: ['income ', 'expense'],
+    required: [true, 'Type is required']
+   },
+   category:{
+    type:String,
+    required: [true, 'Category is required']
+   },
+   date:{
+    type:Date,
+    required:[true,'Date is required'],
+   },
+   description:{
+     type:String,
+   },
+   userId:{
+     type:String,
+    required:[true,'UserId is required'],
+   }
+},{
+    timestamps: true
+})
+
+const Transaction = mongoose.model<ITransaction>('Transaction', transactionSchema);
+
+export default Transaction
